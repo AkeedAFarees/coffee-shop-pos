9dc00d833a6280a3dcebd26ef2fecbadc943a4d3
AkeedAFarees <akeedahmedfarees@gmail.com>
2024-12-20 04:05:31 +0500

    Show flash messages for order placed and redirect to order detail page

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,15 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.addEventListener("turbo:load", () => {
+    // Select the flash message element
+    const flashMessage = document.querySelector('.flash-messages');
+  
+    if (flashMessage) {
+      setTimeout(() => {
+        flashMessage.classList.add('fade-out');
+    }, 2500);
+    }
+  });
+  
