748f5b098b7f4098f84bc52d956286eb2a3fee58
AkeedAFarees <akeedahmedfarees@gmail.com>
2024-12-20 04:06:22 +0500

    Styling for flash messages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_12_19_204322) do
+ActiveRecord::Schema[7.1].define(version: 2024_12_19_222439) do
   create_table "items", force: :cascade do |t|
     t.string "name"
     t.decimal "price", precision: 8, scale: 2
@@ -39,6 +39,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_19_204322) do
     t.string "status", default: "pending"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "placed_at"
+    t.datetime "completed_at"
     t.index ["store_id"], name: "index_orders_on_store_id"
   end
 
