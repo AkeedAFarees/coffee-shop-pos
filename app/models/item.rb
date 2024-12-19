d447ae28f1e5673813bd0e736540996356e4d1e0
AkeedAFarees <akeedahmedfarees@gmail.com>
2024-12-20 02:45:45 +0500

    Add associations for orders and order_items

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,8 @@
 # app/models/item.rb
 class Item < ApplicationRecord
     belongs_to :store
+    has_many :order_items
+    has_many :orders, through: :order_items
   
     validates :name, presence: true
     validates :price, presence: true, numericality: { greater_than: 0 }
--- app/models/store.rb
+++ app/models/store.rb
@@ -1,7 +1,8 @@
 # app/models/store.rb
 class Store < ApplicationRecord
     has_many :items, dependent: :destroy
-
+    has_many :orders, dependent: :destroy
+    
     validates :name, presence: true
     validates :location, presence: true
 end
